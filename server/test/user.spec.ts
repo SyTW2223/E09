807fc787e19d203b747e2e41849f7bdf7353bf47
alu0101321219 <alu0101321219@ull.edu.es>
2022-12-16 09:03:06 +0000

    Renombramos el fichero .mocharc.json

--- server/test/user.spec.ts
+++ server/test/user.spec.ts
@@ -6,17 +6,22 @@ import { User } from '../src/models/user';
  */
 beforeEach(async () => {
     await User.deleteMany();
-  });
+});
 
 /**
  * 
  */
-describe('POST /notes', () => {
-  it('Should successfully insert a new note', async () => {
-    await request(User).post('/notes').send({
-      title: "Note",
-      body: "This is a note",
-      color: "red",
+describe('POST USER /api/signup', () => {
+  it('Should successfully create a new user', async () => {
+    await request(User).post('/api/signup').send({
+      name: "userExample",
+      email: "user@example.com",
+      password: "passwordExample",
+      description: "descriptionExample",
+      folowing: 0,
+      followers: 0,
+      likes: 0,
+      age: 0,
     }).expect(201);
   });
 });
