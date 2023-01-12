a61dae6ee30cb3a9d534b2b5e8d5a14cfc5b31d6
Adrian Gonzalez Galvan <glezgalvan.adrian@gmail.com>
2023-01-12 12:13:25 +0000

    Primer diseño de JuiceModel

--- src/store/modules/juiceModel.js
+++ src/store/modules/juiceModel.js
@@ -2,7 +2,22 @@ import axios from "axios"
 import router from "../../router/index"
 
 export const JuiceModel = {
-    state: () => ({
+  state: () => ({
+    id: 0,
+    text: 'Texto de ejemplo',
+    userName: '',
+    likes: 0,
+  }),
+  mutations: {
 
-    }),
+  },
+  actions: {
+
+  },
+  getters: {
+    id: state => state.id,
+    text: state => state.text,
+    userName: state => state.userName,
+    likes: state => state.likes,
+  }
 }
