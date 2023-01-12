72eb2a671f909706eb3f814063ccd0d6d5a89532
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-12 18:51:03 +0000

    Actualización URL bdd

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -2,7 +2,7 @@ import {connect} from 'mongoose';
 import {remoteUrl} from './dbconfig';
 
 console.log(remoteUrl);
-connect(process.env.DATABASE_URL as string, {
+connect(remoteUrl as string, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
