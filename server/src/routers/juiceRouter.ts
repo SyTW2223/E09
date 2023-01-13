32fa6011dad0094877925a15b14fec105b9acd62
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-13 18:17:13 +0000

    Actualización DELETE publicaciones

--- server/src/routers/juiceRouter.ts
+++ server/src/routers/juiceRouter.ts
@@ -15,3 +15,8 @@ juiceRouter.get('/api/juices', (req, res) => {
   const filter = req.query.id?{name: req.query.id.toString()}:{};
   AppCRUD.get(res, filter, Juice);
 });
+
+// DELETE
+juiceRouter.delete('/api/juices', (req, res) => {
+  AppCRUD.delete(req, res, Juice);
+});
