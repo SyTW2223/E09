aad5aadbfc1135740ae409cada73870aab438dbb
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-21 20:38:12 +0000

    Configuración vue para el despliegue en Github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/E09/" : "/",
   //devServer: {
     //host: '172.16.118.20',
     //port: '3000'
