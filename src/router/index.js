248313b1f53979bf84937468b30a5520b5d3d9d4
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-21 22:22:35 +0000

    Configuración vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/homeView.vue'
 import SignInView from '../views/signinView.vue'
 import SignUpView from '../views/signupView.vue'
@@ -6,48 +6,43 @@ import ForgotView from '../views/forgotView.vue'
 import ResetView from '../views/resetView.vue'
 import ProfileView from '../views/profileView.vue'
 
-const mode = 'history';
+//const mode = 'history';
 
 const routes = [
   {
     path: '/',
-    redirect: {name: 'Home'}
-  },
-  {
-    path: '/E09/',
     name: 'Home',
     component: HomeView
   },
   {
-    path: '/E09/signin',
+    path: '/signin',
     name: 'SignIn',
     component: SignInView
   },
   {
-    path: '/E09/signup',
+    path: '/signup',
     name: 'SignUp',
     component: SignUpView
   },
   {
-    path: '/E09/forgot',
+    path: '/forgot',
     name: 'Forgot',
     component: ForgotView
   },
   {
-    path: '/E09/reset/:token',
+    path: '/reset/:token',
     name: 'Reset',
     component: ResetView
   },
   {
-    path: '/E09/profile/:userName',
+    path: '/profile/:userName',
     name: 'Profile',
     component: ProfileView
   }
 ]
 const router = createRouter({
   //history: createWebHistory(process.env.BASE_URL),
-  //history: createWebHistory('/E09/'),
-  routes,
-  mode
+  history: createWebHistory(),
+  routes
 })
 export default router
