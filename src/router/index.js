a34c6866f415f031e2c5704d33cc33a1e229ca72
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-21 22:03:54 +0000

    Configuración vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -10,32 +10,32 @@ const mode = 'history';
 
 const routes = [
   {
-    path: '/',
+    path: '/E09/',
     name: 'Home',
     component: HomeView
   },
   {
-    path: '/signin',
+    path: '/E09/signin',
     name: 'SignIn',
     component: SignInView
   },
   {
-    path: '/signup',
+    path: '/E09/signup',
     name: 'SignUp',
     component: SignUpView
   },
   {
-    path: '/forgot',
+    path: '/E09/forgot',
     name: 'Forgot',
     component: ForgotView
   },
   {
-    path: '/reset/:token',
+    path: '/E09/reset/:token',
     name: 'Reset',
     component: ResetView
   },
   {
-    path: '/profile/:userName',
+    path: '/E09/profile/:userName',
     name: 'Profile',
     component: ProfileView
   }
