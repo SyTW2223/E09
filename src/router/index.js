c05e62236101dc35690f489bed48b9d5d38e8d74
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-01-21 21:07:04 +0000

    Configuración vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -41,7 +41,8 @@ const routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  //history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/E09/'),
   routes,
   mode
 })
